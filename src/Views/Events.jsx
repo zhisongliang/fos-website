ba3401097fd80da7325575439f853255589f43f4
ChrisLiang7 <zliang15@calpoly.edu>
2023-10-19 09:48:51 -0700

    update events

--- src/Views/Events.jsx
+++ src/Views/Events.jsx
@@ -23,17 +23,13 @@ const Events = () => {
 
             <Col lg={4} className="col-description">
               <div>
-                <h3 style={{ fontWeight: "normal" }}>
-                  Celebration of Lunar New Year 2022.
-                </h3>
+                <h3 style={{ fontWeight: "normal" }}>2023新生入学迎新项目</h3>
                 <p>
                   {" "}
-                  A big shoutout to all performers, facilitators, organizers and
-                  ISI team who hosted and cooked delicious food for all of the
-                  us.
+                  每年的秋季学期，我们都会举办迎新活动，目的是给初来乍到的同学们，提供答疑解惑的项目讲座，包括如何选课、选专业，如何准备转学、文书，如何利用学校的资源等等。我们有很多经验丰富的学长学姐在这里给予你课内课外的支持，欢迎新生加入我们的大家庭。
                 </p>
-                <a href="https://www.instagram.com/deanza_fos/">
-                  <span className="button button-hero">Find out more</span>
+                <a href="https://www.facebook.com/groups/292290343474">
+                  <span className="button button-hero">加入我们的FB Group</span>
                 </a>
               </div>
             </Col>
@@ -52,8 +48,10 @@ const Events = () => {
                     There was a variety of food and dessert from different
                     countries.
                   </p>
-                  <a href="https://www.facebook.com/groups/292290343474">
-                    <span className="button button-hero">Discover more</span>
+                  <a href="https://www.instagram.com/deanza_fos/">
+                    <span className="button button-hero">
+                      关注我们的Instagram
+                    </span>
                   </a>
                 </div>
               </Col>
@@ -117,9 +115,7 @@ const Events = () => {
                   will see the wonders of this extraordinary National Estuary.{" "}
                 </p>
                 <a href="https://www.facebook.com/groups/292290343474/">
-                  <span className="button button-hero">
-                    Join our Facebook group
-                  </span>
+                  <span className="button button-hero">了解更多</span>
                 </a>
               </div>
             </Col>
